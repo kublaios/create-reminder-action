7c1796c4e4bb98db04014568f107e228d6b7bbf0
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2023-08-20 14:25:26 +0200

    Ditch `parse-reminder` dependency and use custom logic.

--- utilities.js
+++ utilities.js
@@ -1,9 +1,78 @@
-const parseReminder = require('parse-reminder');
+function calculateTime(num, unit) {
+  const now = new Date();
+  let time = now.getTime();
 
-///{ who: 'me',
-//   what: 'call the doctor',
-//   when: 2017-09-12T12:00:00.000Z }
-function getReminder(context, referenceDate = null) {
+  switch (unit) {
+    case 'minutes':
+    case 'minute':
+      time += num * 60000;
+      break;
+    case 'hours':
+    case 'hour':
+      time += num * 3600000;
+      break;
+    case 'days':
+    case 'day':
+      time += num * 86400000;
+      break;
+    case 'months':
+    case 'month':
+      time = new Date(
+        now.getFullYear(),
+        now.getMonth() + num,
+        now.getDate()
+      ).getTime();
+      break;
+    case 'years':
+    case 'year':
+      time = new Date(
+        now.getFullYear() + num,
+        now.getMonth(),
+        now.getDate()
+      ).getTime();
+      break;
+  }
+
+  return new Date(time);
+}
+
+function parseReminder(prompt) {
+  // remind {who} to {what} {when}
+  const regex1 =
+    /remind\s+(?:@?([\w-]+))?\s+to\s+(.+(?=\sin|\safter))\s*(?:(?:in|after)\s+(\d+)\s+(minutes?|hours?|days?|months?|years?)?)?/i;
+  // remind {who} {when} to {what}
+  const regex2 =
+    /remind\s+(?:@?([\w-]+))?\s*(?:(?:in|after)\s+(\d+)\s+(minutes?|hours?|days?|months?|years?)?)?\s+to\s+(.+)/i;
+
+  const match1 = prompt.match(regex1);
+  const match2 = prompt.match(regex2);
+
+  if (!match1 && !match2) {
+    throw new Error(
+      "Invalid reminder format. Please use the format: 'remind {who} to {what} {when}' or 'remind {who} {when} to {what}'"
+    );
+  }
+
+  let who, what, num, unit;
+
+  if (match1) {
+    who = match1[1] ? match1[1] : 'me';
+    what = match1[2].trim();
+    num = match1[3] ? Number(match1[3]) : 0;
+    unit = match1[4] ? match1[4].toLowerCase() : 'minutes';
+  } else {
+    who = match2[1] ? match2[1] : 'me';
+    what = match2[4].trim();
+    num = match2[2] ? Number(match2[2]) : 0;
+    unit = match2[3] ? match2[3].toLowerCase() : 'minutes';
+  }
+
+  const when = calculateTime(num, unit);
+
+  return { who, what, when };
+}
+
+function getReminder(context) {
   const body = context.comment.body;
   let remindLine = null;
   let inCode = false;
@@ -32,7 +101,7 @@ function getReminder(context, referenceDate = null) {
   let prompt = remindLine.slice(1);
   console.log('prompt ', prompt);
   console.log('current date and time', new Date());
-  const reminder = parseReminder(prompt, referenceDate);
+  const reminder = parseReminder(prompt);
 
   if (!reminder) {
     throw new Error(`Unable to parse reminder: remind ${body}`);
